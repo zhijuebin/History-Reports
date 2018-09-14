62db28c46839565e98cddd9758f983aa59a70210
zhijuebin <zhijuebin@foxmail.com>
2018-09-14 13:33:26 +0800

    添加Account name列; 添加筛掉physical_odometer_km为None的;添加小于0的数量统计

--- scripts/exception_user_statistics.py
+++ scripts/exception_user_statistics.py
@@ -51,7 +51,7 @@ def connect_to_database():
 
 
 def get_user(csv_path):
-    user_cols = ['id', 'first_name', 'last_name', 'email']
+    user_cols = ['id', 'first_name', 'last_name', 'email', 'account_id']
     sql = '''
         select {} from vt_user;
     '''.format(reduce(lambda x, y: x + ',' + y, user_cols))
@@ -78,6 +78,20 @@ def get_history(csv_path):
     return history_df
 
 
+def get_account(csv_path):
+    account_cols = ['id', 'name']
+    sql = '''
+        select {} from account;
+    '''.format(reduce(lambda x, y: x + ',' + y, account_cols))
+    cur.execute(sql)
+    sql_return = cur.fetchall()
+    account_df = pd.DataFrame(sql_return, columns=account_cols)
+
+    account_df.to_csv(csv_path)
+
+    return account_df
+
+
 class GenerateExceptionUserCsv(object):
 
     def _fun_if_unusual(self, x):
@@ -89,11 +103,27 @@ class GenerateExceptionUserCsv(object):
         return 1 if float(physical_odometer_km) <= 0 else 0
 
 
+    def _fun_under_zero(self, x):
+        according = x.split('||')
+        physical_odometer_km = according[0]
+        odometer_km = according[1]
+        if physical_odometer_km == '':
+            return 1 if float(odometer_km) <= 0 else 0
+        return 1 if float(physical_odometer_km) < 0 else 0
+
+
     def _cal_proportion(self, x):
         logs_number = x.split('||')[0]
         unusual_number = x.split('||')[1]
 
         return str(round(float(unusual_number) / float(logs_number), 2) * 100) + '%' if float(logs_number) != 0 else '-'
+
+    def _cal_under_zero_proportion(self, x):
+        logs_number = x.split('||')[0]
+        under_zero_number = x.split('||')[1]
+
+        return str(round(float(under_zero_number) / float(logs_number), 2) * 100) + '%' if float(
+            logs_number) != 0 else '-'
 
 
     def _process_add_unusual_according(self):
@@ -103,29 +133,38 @@ class GenerateExceptionUserCsv(object):
 
     def _process_add_unusual_flag(self):
         self.history_df['if_unusual'] = self.history_df.if_unusual_according.apply(lambda x: self._fun_if_unusual(x))
+
+
+    def _process_add_under_zero_flag(self):
+        self.history_df['under_zero'] = self.history_df.if_unusual_according.apply(lambda x: self._fun_under_zero(x))
         self.history_cp_df = copy.deepcopy(self.history_df)
 
 
-    def _process_add_logs_unusual_number(self):
+    def _process_add_logs_unusual_and_under_zero_number(self):
         self.history_df = self.history_df.groupby(['user_id'], as_index=True).apply(
             lambda x: pd.Series({
                 'logs_number': int(len(x.user_id)),
-                'unusual_number': int(sum(x.if_unusual))
+                'unusual_number': int(sum(x.if_unusual)),
+                'under_zero_number': int(sum(x.under_zero))
             })
         ).reset_index()
 
 
-    def _combine_history_user_df(self):
-        self.combine_df = self.history_df.merge(self.user_df, how='left', left_on='user_id', right_on='id')
+    def _combine_history_user_account_df(self):
+        self.combine_df = self.history_df.merge(self.user_account_df, how='left', left_on='user_id', right_on='id')
 
 
     def _process_combine_df_add_proportion_according(self):
         self.combine_df['proportion_according'] = self.combine_df.logs_number.apply(
             lambda x: str(x)) + self.combine_df.unusual_number.apply(lambda x: '||' + str(x))
 
+        self.combine_df['proportion_under_zero_according'] = self.combine_df.logs_number.apply(
+            lambda x: str(x)) + self.combine_df.under_zero_number.apply(lambda x: '||' + str(x))
+
 
     def _process_combine_df_add_proportion(self):
         self.combine_df['proportion'] = self.combine_df.proportion_according.apply(lambda x: self._cal_proportion(x))
+        self.combine_df['under_proportion'] = self.combine_df.proportion_under_zero_according.apply(lambda x: self._cal_proportion(x))
 
 
     def _unusual_df_generate(self):
@@ -139,11 +178,14 @@ class GenerateExceptionUserCsv(object):
 
         self.combine_f_df = self.combine_f_df.groupby(['user_id', 'event_type'], as_index=False).apply(lambda x: pd.Series({
             'email': max(x.email),
+            'account_name': max(x.account_name),
             'first_name': max(x.first_name),
             'last_name': max(x.last_name),
             'logs_number': int(max(x.logs_number)),
             'unusual_number': int(max(x.unusual_number)),
             'unusual_proportion': max(x.proportion),
+            'under_zero_number': int(max(x.under_zero_number)),
+            'under_zero_proportion': max(x.under_proportion),
 
             'event_number': len(x.event_type) if max(x.event_type) != '-' else '-',
             'event_proportion': '-' if max(x.event_type) == '-' else str(
@@ -153,28 +195,43 @@ class GenerateExceptionUserCsv(object):
 
     def _sorted_columns_of_combine_f_df(self):
         self.combine_f_df = self.combine_f_df[
-            ['email', 'first_name', 'last_name', 'logs_number', 'unusual_number', 'unusual_proportion', 'event_type',
-             'event_number', 'event_proportion']]
+            ['email', 'account_name', 'first_name', 'last_name', 'logs_number', 'unusual_number', 'unusual_proportion',
+             'under_zero_number', 'under_zero_proportion', 'event_type', 'event_number', 'event_proportion']]
 
 
     def _rename_columns_of_combine_f_df(self):
         self.combine_f_df = self.combine_f_df.rename(columns={
             'email': 'Email',
+            'account_name': 'Account name',
             'first_name': 'First name',
             'last_name': 'Last name',
             'logs_number': 'Logs',
             'unusual_number': 'Exceptions',
             'unusual_proportion': 'Exceptions/Logs',
+            'under_zero_number': 'Exceptions under zero',
+            'under_zero_proportion': 'Except UnZero/Logs',
             'event_type': 'Exceptions type',
             'event_number': 'Exceptions type num',
             'event_proportion': 'Exceptions Type/Logs'
         })
 
 
-    def __init__(self, csv_path, user_df, history_df):
+    def __init__(self, csv_path, user_df, history_df, account_df):
         self.csv_path = csv_path
         self.user_df = user_df
+
         self.history_df = history_df
+        self.history_df = self.history_df[self.history_df.physical_odometer_km.apply(lambda x: x == x)]
+
+        self.account_df = account_df
+
+        self.user_account_df = self.user_df.merge(self.account_df, how='left', left_on='account_id', right_on='id')
+        self.user_account_df = self.user_account_df.rename(columns={
+            'id_x': 'id',
+            'name': 'account_name'
+        })
+        self.user_account_df['account_name'] = self.user_account_df.account_name.apply(lambda x: x if x==x else '-')
+
         self.columns = ['Email', 'First name', 'Last name', 'Logs', 'Exceptions', 'Exceptions/Logs', 'Exceptions type', 'Exceptions type num', 'Exceptions Type/Logs']
         self.history_cp_df = None
         self.combine_df = None
@@ -189,9 +246,11 @@ class GenerateExceptionUserCsv(object):
 
         self._process_add_unusual_according()
         self._process_add_unusual_flag()
-        self._process_add_logs_unusual_number()
+        self._process_add_under_zero_flag()
+        self._process_add_logs_unusual_and_under_zero_number()
 
-        self._combine_history_user_df()
+
+        self._combine_history_user_account_df()
         self._process_combine_df_add_proportion_according()
         self._process_combine_df_add_proportion()
 
@@ -221,6 +280,10 @@ if __name__ == '__main__':
     if os.path.exists(history_csv_path):
         os.remove(history_csv_path)
 
+    account_csv_path = '../data/csv/account.csv'
+    if os.path.exists(account_csv_path):
+        os.remove(account_csv_path)
+
 
     connect_to_database()
 
@@ -240,10 +303,18 @@ if __name__ == '__main__':
         else:
             break
 
+    while True:
+        try:
+            account_df = get_account(account_csv_path)
+        except Exception as e:
+            connect_to_database()
+        else:
+            break
+
     history_last_1_days_df = history_last_7_days_df[history_last_7_days_df.create_date.apply(lambda x: str(x)[:10] == now_str)]
 
     csv_last_7_path = '../data/csv/{} Last 7 days activity users.csv'.format(now_end_str)
     csv_last_1_path = '../data/csv/{} activity users.csv'.format(now_end_str)
 
-    GenerateExceptionUserCsv(csv_path=csv_last_7_path, user_df=user_df, history_df=history_last_7_days_df)()
-    GenerateExceptionUserCsv(csv_path=csv_last_1_path, user_df=user_df, history_df=history_last_1_days_df)()
+    GenerateExceptionUserCsv(csv_path=csv_last_7_path, user_df=user_df, history_df=history_last_7_days_df, account_df=account_df)()
+    GenerateExceptionUserCsv(csv_path=csv_last_1_path, user_df=user_df, history_df=history_last_1_days_df, account_df=account_df)()
